70af494a90df11a3becf617e4b6c2fd10beac20e
DallonF <dallon.feldner@gmail.com>
2018-07-27 08:34:32 -0500

    Load and play music from the local filesystem

--- src/audio-tests.js
+++ src/audio-tests.js
@@ -1,24 +1,13 @@
-import testmusicUrl from './testmusic.mp3';
+import { fromCallback } from 'promise-fns';
+const fs = window.require('fs');
+const FILE_PATH = process.env.REACT_APP_DEFAULT_MUSIC_FILE;
 
 export async function start() {
   const context = new AudioContext();
 
-  const musicBufferPromise = new Promise((resolve, reject) => {
-    const req = new XMLHttpRequest();
-    req.open('GET', testmusicUrl, true);
-    req.responseType = 'arraybuffer';
-
-    req.onload = () => {
-      context.decodeAudioData(
-        req.response,
-        buffer => {
-          resolve(buffer);
-        },
-        reject
-      );
-    };
-    req.send();
-  });
+  const musicBufferPromise = fromCallback(cb =>
+    fs.readFile(FILE_PATH, cb)
+  ).then(buffer => console.log('loaded', buffer.buffer) || context.decodeAudioData(buffer.buffer));
 
   const src = context.createBufferSource();
   src.buffer = await musicBufferPromise;
