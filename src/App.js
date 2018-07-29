7293a19e797b37daa185bae6a691feef6e34b884
DallonF <dallon.feldner@gmail.com>
2018-07-29 15:21:30 -0500

    Switch between two different tracks

--- src/App.js
+++ src/App.js
@@ -5,25 +5,31 @@ import PlayerContext from './structure/PlayerContext';
 class App extends Component {
   constructor(...args) {
     super(...args);
-    this.musicTrack = new MusicTrack(
+    this.musicTrack1 = new MusicTrack(
       process.env.REACT_APP_DEFAULT_MUSIC_FILE,
+      this.props.player.audioContext
+    );
+    this.musicTrack2 = new MusicTrack(
+      process.env.REACT_APP_FADE_TO_MUSIC_FILE,
       this.props.player.audioContext
     );
   }
 
   componentDidMount() {
-    this.musicTrack.preload();
+    this.musicTrack1.preload();
+    this.musicTrack2.preload();
   }
 
   render() {
     return (
       <div>
-        <button onClick={() => this.props.player.play(this.musicTrack)}>
-          Play
+        <button onClick={() => this.props.player.play(this.musicTrack1)}>
+          Play 1
         </button>
-        <button onClick={() => this.props.player.stop()}>
-          Stop
+        <button onClick={() => this.props.player.play(this.musicTrack2)}>
+          Play 2
         </button>
+        <button onClick={() => this.props.player.stop()}>Stop</button>
       </div>
     );
   }
