01c463ade73531f9fe574ccc7dd78526c5c7e1d9
DallonF <dallon.feldner@gmail.com>
2018-08-14 08:41:52 -0500

    You now pick music from the filesystem

--- src/App.js
+++ src/App.js
@@ -2,35 +2,36 @@ import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import MusicTrack from './logic/MusicTrack.js';
 import PlayerContext from './structure/PlayerContext';
+const { dialog } = window.require('electron').remote;
 
 const App = observer(
   class App extends Component {
     constructor(...args) {
       super(...args);
-      this.musicTrack1 = new MusicTrack(
-        process.env.REACT_APP_DEFAULT_MUSIC_FILE,
-        this.props.player.audioContext
-      );
-      this.musicTrack2 = new MusicTrack(
-        process.env.REACT_APP_FADE_TO_MUSIC_FILE,
-        this.props.player.audioContext
-      );
+      this.state = { next: null };
     }
 
-    componentDidMount() {
-      this.musicTrack1.preload();
-      this.musicTrack2.preload();
-    }
+    handleChooseNext = () => {
+      const result = dialog.showOpenDialog(null, {
+        filters: [{ name: 'Music', extensions: ['mp3', 'wav', 'ogg'] }],
+        properties: ['openFile'],
+      });
+      if (result && result.length) {
+        const track = new MusicTrack(result[0], this.props.player.audioContext);
+        this.setState({ next: track });
+      }
+    };
 
     render() {
+      const { next } = this.state;
       return (
         <div>
-          <button onClick={() => this.props.player.play(this.musicTrack1)}>
-            Play {this.musicTrack1.name}
-          </button>
-          <button onClick={() => this.props.player.play(this.musicTrack2)}>
-            Play {this.musicTrack2.name}
-          </button>
+          <button onClick={this.handleChooseNext}>Pick track</button>
+          {next && (
+            <button onClick={() => this.props.player.play(next)}>
+              Play {next.name}
+            </button>
+          )}
           <button onClick={() => this.props.player.fadeOutAndStop()}>
             Stop
           </button>
