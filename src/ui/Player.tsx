36643cf6b833a6e35b2e7b9233eef5293fa41660
DallonF <dallon.feldner@gmail.com>
2019-06-12 18:25:11 -0500

    Now Playing indicator

--- src/ui/Player.tsx
+++ src/ui/Player.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import { Paper, Theme, Box } from '@material-ui/core';
+import { Paper, Theme, Box, Typography } from '@material-ui/core';
 import { Slider } from '@material-ui/lab';
 import { makeStyles } from '@material-ui/styles';
 import { useAppStateContext } from '../structure/AppStateContext';
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
-    padding: theme.spacing(3, 2),
+    padding: theme.spacing(2, 2, 3, 2),
     overflow: 'hidden',
   },
   layout: {
     display: 'grid',
     gridTemplateAreas:
       "'nowplaying nowplaying time' 'controlbuttons progress progress'",
-    gridTemplateColumns: 'auto 1fr',
+    gridTemplateColumns: 'auto 1fr auto',
+    gridRowGap: theme.spacing(2),
+  },
+  nowplaying: {
+    gridArea: 'nowplaying',
   },
   progress: {
     gridArea: 'progress',
@@ -30,11 +34,21 @@ const Player = observer(() => {
   return (
     <Paper className={classes.root} elevation={5}>
       <Box className={classes.layout}>
+        <Box className={classes.nowplaying}>
+          <Typography variant="subtitle1">
+            {player.currentSound ? (
+              <>
+                <span style={{ fontWeight: 'bold' }}>Now Playing:</span>&nbsp;
+                {player.currentSound.track.name}
+              </>
+            ) : (
+              <>&nbsp;</>
+            )}
+          </Typography>
+        </Box>
         <Box className={classes.progress}>
           <Slider
-            disabled={
-              player.state.type !== 'PLAYING'
-            }
+            disabled={player.state.type !== 'PLAYING'}
             max={
               player.currentSoundDuration == null
                 ? 100
