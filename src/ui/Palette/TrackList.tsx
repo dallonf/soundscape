cc9962443c2b260606f56b4e386f146627f2b67c
DallonF <dallon.feldner@gmail.com>
2019-06-18 21:13:41 -0500

    First implementation of re-ordering (does not yet persist, styles still WIP)

--- src/ui/Palette/TrackList.tsx
+++ src/ui/Palette/TrackList.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { EventHandler, SyntheticEvent } from 'react';
 import { observer } from 'mobx-react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import {
   List,
   ListItem,
@@ -7,6 +8,7 @@ import {
   IconButton,
   ListItemIcon,
   ListItemSecondaryAction,
+  Theme,
 } from '@material-ui/core';
 import {
   PlayArrow as PlayArrowIcon,
@@ -16,6 +18,7 @@ import { useAppStateContext } from '../../structure/AppStateContext';
 import MusicTrack from '../../logic/MusicTrack';
 import Player from '../../logic/Player';
 import Palette from '../../logic/Palette';
+import { makeStyles } from '@material-ui/styles';
 
 const TrackList = observer(() => {
   const appState = useAppStateContext();
@@ -23,30 +26,61 @@ const TrackList = observer(() => {
   const { palette, player } = appState;
 
   return (
-    <List>
-      {palette.tracks.map(track => {
-        return (
-          <TrackListItem
-            key={track.id}
-            track={track}
-            player={player}
-            palette={palette}
-          />
-        );
-      })}
-    </List>
+    <DragDropContext onDragEnd={() => {}}>
+      <Droppable droppableId="palette">
+        {provided => (
+          <List {...provided.droppableProps} ref={provided.innerRef}>
+            {palette.tracks.map((track, index) => {
+              return (
+                <TrackListItem
+                  key={track.id}
+                  track={track}
+                  index={index}
+                  player={player}
+                  palette={palette}
+                />
+              );
+            })}
+            {provided.placeholder}
+          </List>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 });
 
+const composeDomEventHandler = <T extends SyntheticEvent>(
+  ...fns: (EventHandler<T> | undefined | null)[]
+) => {
+  return (e: any) => {
+    fns.forEach(fn => {
+      if (fn) {
+        fn(e);
+      }
+    });
+  };
+};
+
+type DivProps = React.DOMAttributes<HTMLDivElement>;
+
+const useItemStyles = makeStyles((theme: Theme) => ({
+  item: {
+    background: theme.palette.background.default,
+  },
+}));
+
 const TrackListItem = ({
   track,
+  index,
   player,
   palette,
 }: {
   track: MusicTrack;
+  index: number;
   player: Player;
   palette: Palette;
 }) => {
+  const classes = useItemStyles();
   const [isFocused, setFocused] = React.useState(false);
   const [isHovered, setHovered] = React.useState(false);
   const isActive = isFocused || isHovered;
@@ -61,30 +95,54 @@ const TrackListItem = ({
   };
 
   return (
-    <ListItem key={track.id} dense={true} {...focusProps}>
-      <ListItemIcon>
-        <IconButton
-          edge="start"
-          onClick={() => player.play(track)}
-          color={isPlaying ? 'secondary' : undefined}
-        >
-          <PlayArrowIcon />
-        </IconButton>
-      </ListItemIcon>
-      <ListItemText primary={track.name} secondary={track.dirname} />
-      <ListItemSecondaryAction
-        style={{ opacity: isActive ? 1 : 0 }}
-      >
-        <IconButton
-          edge="end"
-          aria-label="Delete"
-          onClick={() => palette.removeTrack(track)}
-          {...focusProps}
-        >
-          <DeleteIcon />
-        </IconButton>
-      </ListItemSecondaryAction>
-    </ListItem>
+    <Draggable draggableId={track.id} index={index}>
+      {provided => {
+        return (
+          (console.log(provided) as any) || (
+            <ListItem
+              key={track.id}
+              className={classes.item}
+              dense={true}
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              {...focusProps}
+              onFocus={composeDomEventHandler(
+                (provided.draggableProps as DivProps).onFocus,
+                (provided.dragHandleProps as DivProps).onFocus,
+                focusProps.onFocus
+              )}
+              onBlur={composeDomEventHandler(
+                (provided.draggableProps as DivProps).onBlur,
+                (provided.dragHandleProps as DivProps).onBlur,
+                focusProps.onBlur
+              )}
+            >
+              <ListItemIcon>
+                <IconButton
+                  edge="start"
+                  onClick={() => player.play(track)}
+                  color={isPlaying ? 'secondary' : undefined}
+                >
+                  <PlayArrowIcon />
+                </IconButton>
+              </ListItemIcon>
+              <ListItemText primary={track.name} secondary={track.dirname} />
+              <ListItemSecondaryAction style={{ opacity: isActive ? 1 : 0 }}>
+                <IconButton
+                  edge="end"
+                  aria-label="Delete"
+                  onClick={() => palette.removeTrack(track)}
+                  {...focusProps}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          )
+        );
+      }}
+    </Draggable>
   );
 };
 
